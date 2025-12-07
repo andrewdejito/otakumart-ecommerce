8fddde4514c418e09172f134c7733c52e4f382bf
khyzdlmnt <khydelmonte@gmail.com>
2025-12-07 23:08:46 +0800

    Added Cart Feature

--- backend/vendor/nette/utils/src/Utils/Finder.php
+++ backend/vendor/nette/utils/src/Utils/Finder.php
@@ -385,7 +385,7 @@ class Finder implements \IteratorAggregate
 			$relativePathname = FileSystem::unixSlashes($file->getRelativePathname());
 			foreach ($searches as $search) {
 				if (
-					"is_$search->mode"(Helpers::IsWindows && $file->isLink() ? $file->getLinkTarget() : $file->getPathname())
+					("is_$search->mode")(Helpers::IsWindows && $file->isLink() ? $file->getLinkTarget() : $file->getPathname())
 					&& preg_match($search->pattern, $relativePathname)
 					&& $this->proveFilters($this->filters, $file, $cache)
 				) {
